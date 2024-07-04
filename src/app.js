7391902caaa0c9613eae823b17e2c487756f8c49
LeandroHernandezSr <l.hernandez071999@gmail.com>
2024-07-04 01:06:17 -0300

    Third commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const contenedorExcusas=document.querySelector(".contenedor-excusas");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generadorExcusa(){
+  const parrafoExcusa=document.querySelector(".parrafo-excusa");
+  parrafoExcusa.remove();
+  
+  let nuevaExcusa=`${who[Math.floor(Math.random()*4)]} ${action[Math.floor(Math.random()*4)]} ${what[Math.floor(Math.random()*4)]} ${when[Math.floor(Math.random()*5)]}`;
+  
+  parrafoExcusa.textContent=nuevaExcusa;
+
+  contenedorExcusas.appendChild(parrafoExcusa);
+}
